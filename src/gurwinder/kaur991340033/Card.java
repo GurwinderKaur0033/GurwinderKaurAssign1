fea490cdf69dfc68ed79050d948ff8925f073bf6
gagan <gagan@DESKTOP-ENO6AA3>
2021-06-05 14:04:41 -0600

    Added suitable comments

--- src/gurwinder/kaur991340033/Card.java
+++ src/gurwinder/kaur991340033/Card.java
@@ -1,12 +1,7 @@
 package gurwinder.kaur991340033;
 
-/**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * gurwinder Kaur
- * Assignment 1
+/**Gurwinder Kaur
+ * Assignment 1, June 5th 2021
  * @author Gurwinder
  */
 public class Card {
--- src/gurwinder/kaur991340033/CardTrick.java
+++ src/gurwinder/kaur991340033/CardTrick.java
@@ -3,11 +3,7 @@ package gurwinder.kaur991340033;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects and then
- * asks the user to pick a card and searches the array of cards for the match to
- * the user's card. To be used as starting code in ICE 1
- *
+/* June 5th 2021
  * @author gurwinder
  */
 public class CardTrick {
